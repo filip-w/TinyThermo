28bf7d88b8b832379afb6c8d0be3cb30cd7b365e
Filip Wie <filip.wieslander@gmail.com>
2022-10-11 20:42:02 +0200

    Using struct for thermocouple channels.
    Signed-off-by: Filip Wie <filip.wieslander@gmail.com>

--- TeenyThermo.ino
+++ TeenyThermo.ino
@@ -8,22 +8,13 @@
 #define UpdateRate 10 //Frontend thermocouple scan rate and CAN-message update rate in Hz.
 #define serialDebug false
 
-struct Thermocouple_channel{
+struct thermocoupleChannel{
    int channelNumber;
-   int roll_no;
-   int chip_select;
+   Adafruit_MAX31855 thermocoupleFrontend;
    FIR<float, 10> fir_avg;
-   Adafruit_MAX31855 thermocouple_frontend;
 };
 
 struct can_frame canMsg1;
-struct can_frame canMsg2;
-MCP2515 mcp2515(10);
-FIR<float, 10> fir_avg1;
-FIR<float, 10> fir_avg2;
-FIR<float, 10> fir_avg3;
-FIR<float, 10> fir_avg4;
-FIR<float, 10> filters[4];
 
 //Chip select pins
 #define MAXCS1          6
@@ -38,70 +29,39 @@ FIR<float, 10> filters[4];
 #define SW_CANBaudRate  3 
 #define SW_Lowpass      2
 
-Adafruit_MAX31855 thermocouple_channels[4] = {MAXCS1,MAXCS2,MAXCS3,MAXCS4};
+MCP2515 mcp2515(CANMODCS);
+
+thermocoupleChannel thermocoupleChannels[4]{
+  {1, MAXCS1},
+  {2, MAXCS2},
+  {3, MAXCS3},
+  {4, MAXCS4},
+};
 
 void setup() {
-  //pinMode(6, OUTPUT); //Chip Select Thermo 2
-  //pinMode(7, OUTPUT); //Chip Select Thermo 3
-  //pinMode(9, OUTPUT); //Chip Select Thermo 4
-  //pinMode(10, OUTPUT); //Chip Select CAN module
-
-  //digitalWrite(6, HIGH);   //Disable Thermo 2
-  //digitalWrite(7, HIGH);   //Disable Thermo 3
-  //digitalWrite(9, HIGH);   //Disable Thermo 4
-  //digitalWrite(10, HIGH);   //Disable CAN module
-
-  float coef_avg[10] = {1., 1., 1., 1., 1., 1., 1., 1., 1., 1.};
-  fir_avg1.setFilterCoeffs(coef_avg);
-  fir_avg2.setFilterCoeffs(coef_avg);
-  fir_avg3.setFilterCoeffs(coef_avg);
-  fir_avg4.setFilterCoeffs(coef_avg);
-
-  canMsg2.can_id  = 0x036;
-  canMsg2.can_dlc = 8;
-  canMsg2.data[0] = 0x0E;
-  canMsg2.data[1] = 0x00;
-  canMsg2.data[2] = 0x00;
-  canMsg2.data[3] = 0x08;
-  canMsg2.data[4] = 0x01;
-  canMsg2.data[5] = 0x00;
-  canMsg2.data[6] = 0x00;
-  canMsg2.data[7] = 0xA0;
 
   Serial.begin(9600);
 
   while (!Serial) delay(1); // wait for Serial on Leonardo/Zero, etc
 
-  Serial.print("Moving Average Filter Gain: ");
-  Serial.println(fir_avg1.getGain());
-
-  Serial.println("MAX31855 test");
   Serial.println("Initializing sensors...");
   
   delay(500); // wait for MAX chip to stabilize
-  for(byte i = 0; i < sizeof(thermocouple_channels)/sizeof(Adafruit_MAX31855) - 1; i++){
-    Serial.println(sizeof(thermocouple_channels));
-    if (!thermocouple_channels[i].begin()) {
+  for(byte i = 0; i < sizeof(thermocoupleChannels[i])/sizeof(thermocoupleChannel); i++){
+    Serial.println("Init of channel: " + i);
+    if (!thermocoupleChannels[i].thermocoupleFrontend.begin()) {
       Serial.println("ERROR. Sensor " + i);
       while (1) delay(10);
     }
   }
+  Serial.println("DONE.");
 
-  // OPTIONAL: Can configure fault checks as desired (default is ALL)
-  // Multiple checks can be logically OR'd together.
-  // thermocouple.setFaultChecks(MAX31855_FAULT_OPEN | MAX31855_FAULT_SHORT_VCC);  // short to GND fault is ignored
-
+  Serial.println("Initializing CAN module...");
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
   mcp2515.setNormalMode();
 
-
   Serial.println("DONE.");
-
-  Serial.print("Delay: ");
-  float delay;
-  delay = float(1)/10;
-  Serial.println(delay,3);
 
   Serial.println("Reading settings switches...");
   pinMode(SW_CANID1, INPUT_PULLUP);
@@ -127,7 +87,6 @@ void setup() {
 
   Serial.println("DONE.");
 
-
   canMsg1.can_id  = canid;
   canMsg1.can_dlc = 8;
   canMsg1.data[0] = 0x00;
@@ -142,20 +101,20 @@ void setup() {
 
 void loop() {
   int canMsgIndex=0;
-  for(byte i = 0; i < sizeof(thermocouple_channels)/sizeof(Adafruit_MAX31855); i++){
+  for(byte i = 0; i < sizeof(thermocoupleChannels)/sizeof(thermocoupleChannels[0]); i++){
     // basic readout test, just print the current temp
     if (serialDebug) {
       Serial.print("Internal Temp ");
       Serial.print(i+1);
       Serial.print(":");
-      Serial.print(thermocouple_channels[i].readInternal());
+      Serial.print(thermocoupleChannels[i].thermocoupleFrontend.readInternal());
       Serial.print(",");
     }
 
-    double c = thermocouple_channels[i].readCelsius();
+    double c = thermocoupleChannels[i].thermocoupleFrontend.readCelsius();
     if (isnan(c)) {
       if (serialDebug) Serial.print(i + " sensor fault(s) detected!");
-      uint8_t e = thermocouple_channels[i].readError();
+      uint8_t e = thermocoupleChannels[i].thermocoupleFrontend.readError();
       if (e & MAX31855_FAULT_OPEN){
         c=0xFFFF;
         if (serialDebug) Serial.print("FAULT: Thermocouple is open - no connections.");
